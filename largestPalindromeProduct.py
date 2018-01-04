1913370b559f17109f3f6248c5b404aa066cc6d0
Hannah Hoover <hannahghoover@gmail.com>
2018-01-03 20:10:25 -0600

    Started largestPalindromeProduct

---
+++ largestPalindromeProduct.py
@@ -0,0 +1,16 @@
+# -----------------------------
+# Project Euler Problem 4: Largest Palindrome Product
+# 		Find the largest palindrom made from the product of two 3-digit numbers
+# -----------------------------
+
+def largestPalindromeProd():
+	number = 0
+
+
+
+
+def main():
+	largestPalindromeProd()
+
+if __name__ == '__main__':
+	main()
