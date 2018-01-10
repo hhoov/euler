34eec5e103009ed88ede828e8a96f998fde8e1ba
Hannah Hoover <hannahghoover@gmail.com>
2018-01-09 23:54:23 -0600

    small edits

--- smallestMultiple.py
+++ smallestMultiple.py
@@ -1,9 +1,13 @@
+# -----------------------------
+# Project Euler Problem 5: Smallest Multiple
+# 		
+# -----------------------------
 
 def smallestMultiple():
 	number = 1
 	divisorsAllSatisfied = 1
 	while divisorsAllSatisfied != 20:
-		if number % divisorsAllSatisfied == 0:
+		if isEvenFactor(number, divisorsAllSatisfied) == True:
 			divisorsAllSatisfied = divisorsAllSatisfied + 1
 			print("Divisors Satisfied: {}".format(divisorsAllSatisfied))
 			print("Number is: {}".format(number))
@@ -14,6 +18,10 @@ def smallestMultiple():
 			continue
 	print("Smallest number is {}".format(number))
 	print("divisorsAllSatisfied is: {}".format(divisorsAllSatisfied))
+
+def isEvenFactor(number, divisorsAllSatisfied):
+	number % divisorsAllSatisfied == 0
+	
 
 def main():
 	smallestMultiple()
